5ba379446c9f4cfd9227380b526cf12f3e95c247
liumapp <liumapp.com@gmail.com>
2018-06-08 20:17:04 +0800

    add tinymce for text editor

--- editor-ui/src/views/index.vue
+++ editor-ui/src/views/index.vue
@@ -1,20 +1,15 @@
 /**
- * @file HelloWorld.vue
+ * @file index.vue
  * @author liumapp
  * @email liumapp.com@gmail.com
  * @homepage http://www.liumapp.com
  * @date 6/8/18
  */
 <template>
-</template>
 
+</template>
 <script>
 export default {
-  name: 'HelloWorld',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    }
-  }
+  name: 'index'
 }
 </script>
