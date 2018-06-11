aba0e237e250a7501fcc499e1666524dc3d21a97
liumapp <liumapp.com@gmail.com>
2018-06-11 16:01:01 +0800

    add controller handle editor content

---
+++ editor-service/src/main/java/com/liumapp/demo/docker/editor/controller/IndexController.java
@@ -0,0 +1,19 @@
+package com.liumapp.demo.docker.editor.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author liumapp
+ * @file IndexController.java
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 6/11/18
+ */
+@RestController
+@RequestMapping("")
+public class IndexController {
+
+
+
+}
