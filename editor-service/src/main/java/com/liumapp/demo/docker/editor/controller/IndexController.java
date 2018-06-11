8252cd0d8f16dce0bcd26b84414f10b2d22c71e7
liumapp <liumapp.com@gmail.com>
2018-06-11 20:38:34 +0800

    need aspose word

--- editor-service/src/main/java/com/liumapp/demo/docker/editor/controller/IndexController.java
+++ editor-service/src/main/java/com/liumapp/demo/docker/editor/controller/IndexController.java
@@ -1,6 +1,7 @@
 package com.liumapp.demo.docker.editor.controller;
 
 import com.alibaba.fastjson.JSON;
+import com.liumapp.convert.html.Html2PDF;
 import com.liumapp.demo.docker.editor.entity.Editor;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +20,8 @@ public class IndexController {
 
     @RequestMapping("")
     public String getContent (@RequestBody Editor editor) {
+        Html2PDF html2PDF = new Html2PDF();
+        html2PDF.html2pdf("./test.pdf", editor.getContent());
         return JSON.toJSONString("success");
     }
 
